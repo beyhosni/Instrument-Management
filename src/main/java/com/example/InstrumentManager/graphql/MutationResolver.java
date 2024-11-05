dc39b9261a33437207568f701052743dd1e3aaa9
beyhosni <beyhosni@gmail.com>
2024-11-05 10:41:38 +0100

    remove unused comments

--- src/main/java/com/example/InstrumentManager/graphql/MutationResolver.java
+++ src/main/java/com/example/InstrumentManager/graphql/MutationResolver.java
@@ -26,31 +26,26 @@ public class MutationResolver {
     @Autowired
     private MaintenanceService maintenanceService;
 
-    // Mutation pour ajouter un instrument
     @MutationMapping
     public Instrument addInstrument(@Argument String name, @Argument String serialNumber, @Argument Long categoryId) {
         return instrumentService.addInstrument(name, serialNumber, categoryId);
     }
 
-    // Mutation pour mettre à jour un instrument
     @MutationMapping
     public Instrument updateInstrument(@Argument Long id, @Argument String name, @Argument String serialNumber) {
         return instrumentService.updateInstrument(id, name, serialNumber);
     }
 
-    // Mutation pour supprimer un instrument
     @MutationMapping
     public Boolean deleteInstrument(@Argument Long id) {
         return instrumentService.deleteInstrument(id);
     }
 
-    // Mutation pour ajouter une catégorie
     @MutationMapping
     public Category addCategory(@Argument String name) {
         return categoryService.addCategory(name);
     }
 
-    // Mutation pour ajouter un enregistrement de maintenance
     @MutationMapping
     public MaintenanceRecord addMaintenanceRecord(@Argument Long instrumentId, @Argument String maintenanceDate, @Argument String comments) {
         LocalDate date = LocalDate.parse(maintenanceDate);
